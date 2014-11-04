6e82a72f5ee905f2a2564cc7c0bc0a80d1cec84b
ethan <ethan1.jiang@gmail.com>
2014-11-04 16:15:48 +0800

    add active_admin

--- http_server/db/seeds.rb
+++ http_server/db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+def seed_user arg
+  arg[:password] = "12345678" unless arg[:password]
+  arg[:password_confirmation] = arg[:password]
+  arg[:name] = "nobody" unless arg[:name]
+  arg[:email] = "nobody@nowhere.com" unless arg[:email]
+  u = User.find_or_create_by_name arg
+  puts "  user #{arg[:email]}"
+  u
+end
+
+#u0 = seed_user :name => "admin@btc.com", :email => "admin@btc.com", :password => "adminx", :password_confirmation => "adminx"
+
+
+User.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') 
